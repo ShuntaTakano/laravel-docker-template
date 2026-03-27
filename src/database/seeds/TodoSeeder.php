8769ffcdf74d24f856156ccc8ba56233ccbddd1b
ShuntaTakano <asuna2346@gmail.com>
2026-03-27 16:28:31 +0900

    add: TodoSeederによるテストデータ投入処理を追加

---
+++ src/database/seeds/TodoSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TodoSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('todos')->truncate(); // 全削除
+
+        $testData = [
+            [
+                'content' => 'PHP Appセクションを終える',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'content' => 'Laravel Lessonを終える',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        DB::table('todos')->insert($testData);
+    }
+}
