11363b085574c25a851d42e65a120a75335d50a1
ShuntaTakano <asuna2346@gmail.com>
2026-04-10 22:41:36 +0900

    refactor: 名前付きルートとBladeのレイアウト共通化を実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,4 +18,4 @@
 
 
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create'); // 追加
+Route::get('/todo/create', 'TodoController@create')->name('todo.create'); // 追加
