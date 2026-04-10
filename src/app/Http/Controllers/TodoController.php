ab1735b824e1245521d07cf3f0d1437aa621888a
ShuntaTakano <asuna2346@gmail.com>
2026-04-10 23:21:25 +0900

    add: Todo新規作成処理（登録・リダイレクト）を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Todo;
 
+
 class TodoController extends Controller
 {
     public function index()
@@ -18,5 +19,16 @@ class TodoController extends Controller
     public function create()
     {
         return view('todo.create');
+    }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content');
+
+        $todo = new Todo();
+        $todo->content = $content;
+        $todo->save();
+
+        return redirect()->route('todo.index');
     }
 }
--- src/resources/views/todo/create.blade.php
+++ src/resources/views/todo/create.blade.php
@@ -6,14 +6,21 @@
       <div class="card">
         <div class="card-header">ToDo作成</div>
         <div class="card-body">
-          <form method="" action="">
+          <form method="POST" action="{{ route('todo.store') }}">
+            @csrf
             <div class="form-group row">
               <label class="col-md-4 col-form-label text-md-right">ToDo入力</label>
               <div class="col-md-6">
                 <input type="text" class="form-control" name="content">
               </div>
             </div>
-            <button type="submit" class="btn btn-primary">作成</button>
+
+            <div class="form-group row mb-0">
+              <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">作成</button>
+              </div>
+            </div>
+
           </form>
         </div>
       </div>
